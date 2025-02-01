5ebd1c51925c3c38f94d0bd763f4a3b0999024ec
Sneha Reddy <snehareddy9178@gmail.com>
2025-02-01 23:30:28 +0530

    add navbar and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Navbar from './components/common/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
