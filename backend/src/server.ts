35437b86f045a90a1308997b42a39a2487bc10d1
Sneha Reddy <snehareddy9178@gmail.com>
2025-02-01 21:22:58 +0530

    feat: setup backend server

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import { dbConfig } from './config/db/db.config';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.development.local` });
+dbConfig.init();
+
+import express from 'express';
+import cors from 'cors';
+import { authRouter } from './routes/auth.router';
+import passport from './config/passport';
+import { authMiddleware } from './middlewares/auth.middleware';
+import { session } from './config/session';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/v1/auth', authRouter);
+
+app.use(authMiddleware);
+
+app.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Listening to port ${process.env.PORT ?? 3000}`);
+});
